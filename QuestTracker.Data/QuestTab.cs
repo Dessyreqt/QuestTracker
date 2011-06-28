317b56301412e06d04f5f2bb80afd3e125ba4451
Dessyreqt_cp <SND\Dessyreqt_cp>
2011-06-28 14:29:23 +0000

    QuestTab was missing from project for some reason

---
+++ QuestTracker.Data/QuestTab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace QuestTracker.Data
+{
+    public class QuestTab
+    {
+        public string Name { get; set; }
+        [XmlArrayItem("QuestGroup", typeof(QuestGroup))]
+        public List<QuestGroup> Groups { get; set; }
+
+        public QuestTab()
+        {
+            Groups = new List<QuestGroup>();
+        }
+    }
+}
